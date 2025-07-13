91d8c01f5e947e1a6a789f449e8c47f30aa80ddd
Andreas <andiburger@gmail.com>
2025-07-13 21:21:05 +0200

    some changes to support minecraft log

--- start_log_detective_docker.sh
+++ start_log_detective_docker.sh
@@ -8,6 +8,7 @@ docker run -d \
   -v /var/log/auth.log:/var/log/auth.log:ro \
   -v /var/log/ufw.log:/var/log/ufw.log:ro \
   -v /var/log/fail2ban.log:/var/log/fail2ban.log:ro \
+  -v /var/lib/docker/volumes/minecraft_logs/_data/latest.log:/var/log/minecraft_latest.log:ro \
   -v /etc/log_detective/geoip:/usr/share/GeoIP:ro \
   -v "$(pwd)/config.yaml:/app/config.yaml:ro" \
   -v "$(pwd)/rules:/app/rules:ro" \
